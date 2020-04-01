0a44960820ae952d25e0df0d14c48bfe3e04f523
Artem <starist2000@mail.ru>
2020-04-01 19:52:40 +0300

    Fix 2

--- timing.py
+++ timing.py
@@ -1,22 +1,17 @@
 import matplotlib.pyplot as plt
 
+plt.title('Time/count')
+plt.xlabel('Count, x * 10^4')
+plt.ylabel('Time, s')
+plt.grid()
 lines = [10000, 100000]
 number = [1, 10]
 REG = [0.03663729999999976, 0.36030850000000214]
 SMC = [0.46962290000000007, 4.8826401]
 PLY = [0.3116515999999998, 3.1611903000000003]
-
-plt.title('Time/count')
-plt.xlabel('Count, x * 10^4')
-plt.ylabel('Time, s')
-plt.grid()
-
 plt.plot(number, REG, label='REG')
 plt.plot(number, SMC, label='SMC')
 plt.plot(number, PLY, label='PLY')
 
-
-
 plt.legend()
-
 plt.show()
